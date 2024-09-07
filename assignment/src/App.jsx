2555358b61e10ce547f2ac69ff8f425eaca3e89b
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-09-07 11:22:10 +0000

    assignment

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -2,33 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [activeButton, setActiveButton] = useState(null)
+  const [activeButton, setActiveButton] = useState(null);
 
-  function buttonClick(buttonId){
-    setActiveButton(buttonId)
+  const buttonClicked = (buttonId) => {
+    setActiveButton(buttonId);
   }
-
 
   return (
     <>
-    <div id="left"></div>
-    <div id="right">
-      <div id="top-right">
-        <div className='ques-rect'>
-        <img src="./src/assets/questionmark.png" alt="questionmark image" />
-        <img src="./src/assets/rectangle.png" alt="group of square" />
-        </div>
-        <div className='tab-text'>
-          <div class="tab-container">
-            <button className="tab-button" onClick={() => buttonClick(1)} style={{backgroundColor: activeButton === 1 ? 'grey' : 'transparent'}}>About Me</button>
-            <button className="tab-button" onClick={() => {buttonClick(2)}} style={{backgroundColor}}>Experiences</button>
-            <button className="tab-button" onClick={() => {buttonClick(3)}} style={{backgroundColor}}>Reommended</button>
+      <div id="left"></div>
+      <div id="right">
+        <div id="top-right">
+          <div className='ques-rect'>
+            <img src="./src/assets/questionmark.png" alt="questionmark image" />
+            <img src="./src/assets/rectangle.png" alt="group of square" />
+          </div>
+          <div className='tab-text'>
+            <div class="tab-container">
+              <button className="tab-button" onClick={() => buttonClicked(1)} style={{ backgroundColor: activeButton === 1 ? 'grey' : 'transparent' }}>About Me</button>
+              <button className="tab-button" onClick={() => buttonClicked(2)} style={{ backgroundColor: activeButton === 2 ? 'grey' : 'transparent' }}>Experiences</button>
+              <button className="tab-button" onClick={() => buttonClicked(3)} style={{ backgroundColor: activeButton === 3 ? 'grey' : 'transparent' }}>Reommended</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div id="bottom-right"></div>
-    </div>
+        <div id="bottom-right"></div>
+      </div>
     </>
   )
 }
